60fced6c065058dcfe7cf3475de26d31e0d699bf
HawonNam <ktion1220@naver.com>
2025-05-08 00:50:12 +0900

    mysql 연결, 일부 변수명 변경

--- src/main/java/com/example/CureMap/domain/AntibioticRecommendation.java
+++ src/main/java/com/example/CureMap/domain/AntibioticRecommendation.java
@@ -19,7 +19,7 @@ public class AntibioticRecommendation {
     private Patient patient;
 
     private String antibioticName;     // 추천 항생제 이름
-    private int rank;                  // 추천 순위 (1위, 2위, 3위 등)
+    private int recommandRank;                  // 추천 순위 (1위, 2위, 3위 등)
     private double score;              // 종합 점수 또는 확률 기반 추천 점수
     private String rationale;          // 추천 이유 요약 (예: "과거 사용 이력 없음 + 치료 성공률 87%")
 
